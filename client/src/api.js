f8ea031dab0a37cd11b74c44152c63bb7fc5a531
BBBomber <yashasvi.chopra555@gmail.com>
2023-10-12 23:31:39 +0530

    Project

---
+++ client/src/api.js
@@ -0,0 +1,10 @@
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "35f7044676msh060f063f311d0e5p16a79cjsnaa69971e1d9f",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "2f5f4aa2e9cdec798c2c18a6ef9d0a4c";
